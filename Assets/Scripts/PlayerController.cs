e8b984e50567420cd15de88caf01dada69f9e605
Vladimir Vladimirovich <58000132+Vladimir-MaSTeR@users.noreply.github.com>
2023-07-05 03:53:02 +0700

    Complet one day

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace DefaultNamespace {
+    public class PlayerController : MonoBehaviour {
+        [SerializeField]
+        private GameObject _target;
+
+        private NavMeshAgent _agent;
+
+        private void Start() {
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.destination = _target.transform.position;
+        }
+    }
+}
